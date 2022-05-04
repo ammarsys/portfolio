22d3262c885836fb8f06873bfeeded9a4a37bce4
Ammar <amarftw1@gmail.com>
2022-05-04 14:01:48 +0200

    add email sending functions

---
+++ utils/emails.py
@@ -0,0 +1,25 @@
+# Standard library imports
+
+import smtplib
+
+from email.message import EmailMessage
+
+# Related third party imports
+
+from dotenv import dotenv_values
+
+
+SECRET = dotenv_values('.env')
+
+
+def send_email(name: str, email: str, content: str) -> None:
+    msg = EmailMessage()
+
+    msg.set_content(content)
+    msg['Subject'] = f'{email} | {name}'
+    msg['To'] = "amarftw1@gmail.com"
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(SECRET['PORTFOLIO_EMAIL'], SECRET['PORTFOLIO_PASSWORD'])
+    server.send_message(msg)
+    server.quit()
