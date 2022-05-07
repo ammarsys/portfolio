fb3abff40716af4b87fcc8288d4bfd62e844defc
Ammar <amarftw1@gmail.com>
2022-05-08 01:28:50 +0200

    add the endpoint

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-# Third party / library specific imports
+# Related third party imports
 
-from flask import render_template, Flask
+from flask import Flask, request
+from flask_cors import CORS
+
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+# Local application/library specific imports
+
+from utils import send_email
 
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address
+)
+CORS(app)
 
 
-@app.route('/')
-def index() -> str:
-    return render_template('index.html')
+@app.route('/api/contact', methods=['POST'])
+@limiter.limit(limit_value="2/second")
+def contact() -> dict[str]:
+    values = request.form.to_dict()
+
+    if len(values['name']) > 50 or len(values['email']) > 100 or len(values['content']) > 2000:
+        return {'status': 422, 'request': 'invalid data'}
+
+    send_email(**values)
+    return {'status': 200, 'request': 'OK'}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
