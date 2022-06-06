f251ceb856d1cc6413ffacd405822d73b834091b
Ammar <amarftw1@gmail.com>
2022-06-06 17:57:59 +0200

    add return codes

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ CORS(app)
 
 @app.route('/api/contact', methods=['POST'])
 @limiter.limit(limit_value="2/second")
-def contact() -> dict[str]:
+def contact() -> tuple[dict[str], int]:
     values = request.form.to_dict()
 
     if len(values['name']) > 50 or len(values['email']) > 100 or len(values['content']) > 2000:
-        return {'status': 422, 'request': 'invalid data'}
+        return {'status': 422, 'request': 'invalid data'}, 422
 
     send_email(**values)
-    return {'status': 200, 'request': 'OK'}
+    return {'status': 200, 'request': 'OK'}, 200
 
 
 if __name__ == '__main__':
