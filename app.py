9f41e056877e02244db94a6b1b15dfcb3c32aa5f
Ammar <amarftw1@gmail.com>
2022-06-18 14:45:10 +0200

    formatting black

--- app.py
+++ app.py
@@ -12,29 +12,30 @@ from utils import send_email
 
 
 app = Flask(__name__)
-limiter = Limiter(
-    app,
-    key_func=get_remote_address
-)
+limiter = Limiter(app, key_func=get_remote_address)
 CORS(app)
 
 
-@app.route('/api/contact', methods=['POST'])
+@app.route("/api/contact", methods=["POST"])
 @limiter.limit(limit_value="2/second")
 def contact() -> tuple[dict[str], int]:
     values = request.form.to_dict()
 
-    if len(values['name']) > 50 or len(values['email']) > 100 or len(values['content']) > 2000:
-        return {'status': 422, 'request': 'invalid data'}, 422
+    if (
+        len(values["name"]) > 50
+        or len(values["email"]) > 100
+        or len(values["content"]) > 2000
+    ):
+        return {"status": 422, "request": "invalid data"}, 422
 
     send_email(**values)
-    return {'status': 200, 'request': 'OK'}, 200
+    return {"status": 200, "request": "OK"}, 200
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html'), 200
+    return render_template("index.html"), 200
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- utils/emails.py
+++ utils/emails.py
@@ -9,17 +9,17 @@ from email.message import EmailMessage
 from dotenv import dotenv_values
 
 
-SECRET = dotenv_values('.env')
+SECRET = dotenv_values(".env")
 
 
 def send_email(name: str, email: str, content: str) -> None:
     msg = EmailMessage()
 
     msg.set_content(content)
-    msg['Subject'] = f'{email} | {name}'
-    msg['To'] = "amarftw1@gmail.com"
+    msg["Subject"] = f"{email} | {name}"
+    msg["To"] = "amarftw1@gmail.com"
 
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login(SECRET['PORTFOLIO_EMAIL'], SECRET['PORTFOLIO_PASSWORD'])
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(SECRET["PORTFOLIO_EMAIL"], SECRET["PORTFOLIO_PASSWORD"])
     server.send_message(msg)
     server.quit()
