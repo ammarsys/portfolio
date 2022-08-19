b70bcfff5eaf9b8fee051291c2626aad4e795525
Ammar Hajeer <amarftw1@gmail.com>
2022-08-19 18:37:21 +0200

    fix up js uwu

--- static/js/form.js
+++ static/js/form.js
@@ -9,24 +9,38 @@ function contactMe() {
         "content": document.getElementById("message").value,
         "name": document.getElementById("name").value
     };
+    console.log(Object.values(data).includes(''));
 
-    if (validateEmail(data["email"])) {
-        fetch("/api/contact", {
-            method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data)
-        }).then(_ =>
-            Swal.fire({
-                text: 'Successfully sent your message.',
-            })
-        )
-
-
-    } else {
+    if (Object.values(data).includes('')) {
         Swal.fire({
-            title: 'Bad Information',
-            text: "Looks like your email doesn't seem right, could you double check it?",
-            icon: 'error',
+            text: 'Looks like one of the fields is empty, please fill them in.',
+            background: "#3a435e",
+            color: "#fff"
         })
+    } else {
+        if (validateEmail(data["email"])) {
+            fetch("/api/contact", {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            }).then(_ =>
+                Swal.fire({
+                    text: 'Successfully sent your message.',
+                    icon: "success",
+                    background: "#3a435e",
+                    color: "#fff"
+                })
+            )
+
+
+        } else {
+            Swal.fire({
+                title: 'Bad Information',
+                text: "Looks like your email doesn't seem right, could you double check it?",
+                icon: 'error',
+                background: "#3a435e",
+                color: "#fff"
+            })
+        }
     }
 }
